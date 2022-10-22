4b36fc208743d1c789a25d07964a90c3495fe9ef
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-10-22 11:08:25 -0500

    completed chapter 8

--- views/customer_requests.py
+++ views/customer_requests.py
@@ -1,3 +1,7 @@
+import sqlite3
+import json
+from models import Customer
+
 CUSTOMERS = [
   {
     "id": 1,
@@ -9,7 +13,48 @@ CUSTOMERS = [
 ]
 
 def get_all_customers():
-  return CUSTOMERS
+#   return CUSTOMERS
+  with sqlite3.connect("./kennel.sqlite3") as conn:
+
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.name,
+            c.address,
+            c.email,
+            c.password
+        FROM customer c
+        """)
+
+        # Initialize an empty list to hold all customer representations
+        customers = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Animal class above.
+            customer = Customer(row['id'], row['name'], row ['address'],
+                            row['email'], row['password'])
+
+            customers.append(customer.__dict__)
+
+    # Use `json` package to properly serialize list as JSON
+        return json.dumps(customer)
+  
+
+
+
 
 def get_single_customer(id):
   requested_customer = None
