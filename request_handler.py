b66fbe95a735c9621f1a07982ad3486846154bf7
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-10-24 18:18:53 -0500

    completed chapter 10

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
 import json
-from views import get_all_animals, get_single_animal, get_single_location, get_all_locations, create_animal, get_all_customers, get_single_customer, get_all_employees, get_single_employee, create_location, create_employee, create_customer, delete_animal, delete_location, delete_customer, delete_employee, update_animal, update_location, update_customer, update_employee
+from views import get_all_animals, get_single_animal, get_single_location, get_all_locations, create_animal, get_all_customers, get_single_customer, get_all_employees, get_single_employee, create_location, create_employee, create_customer, delete_animal, delete_location, delete_customer, delete_employee, update_animal, update_location, update_customer, update_employee, get_customers_by_email, get_animals_by_location, get_employees_by_location, get_animals_by_status
 
 
 # Here's a class. It inherits from another class.
@@ -43,40 +44,54 @@ class HandleRequests(BaseHTTPRequestHandler):
     # It handles any GET request.
     def do_GET(self):
         self._set_headers(200)
-        response = {}  # Default response
 
-        # Parse the URL and capture the tuple that is returned
-        (resource, id) = self.parse_url(self.path)
+        response = {}
 
-        if resource == "animals":
-            if id is not None:
-                response = f"{get_single_animal(id)}"
+        # Parse URL and store entire tuple in a variable
+        parsed = self.parse_url(self.path)
 
-            else:
-                response = f"{get_all_animals()}"
-                
-        if resource == "locations":
-            if id is not None:
-                response = f"{get_single_location(id)}"
+        # If the path does not include a query parameter, continue with the original if block
+        if '?' not in self.path:
+            ( resource, id ) = parsed
 
-            else:
-                response = f"{get_all_locations()}"
+            if resource == "animals":
+                if id is not None:
+                    response = f"{get_single_animal(id)}"
+                else:
+                    response = f"{get_all_animals()}"
+            elif resource == "customers":
+                if id is not None:
+                    response = f"{get_single_customer(id)}"
+                else:
+                    response = f"{get_all_customers()}"
+            elif resource == "employees":
+                if id is not None:
+                    response = f"{get_single_employee(id)}"
+                else:
+                    response = f"{get_all_employees()}"
+            elif resource == "location":
+                if id is not None:
+                    response = f"{get_single_location(id)}"
+                else:
+                    response = f"{get_all_locations()}"
+                    
+        else: # There is a ? in the path, run the query param functions
+            (resource, query) = parsed
+            
+            # see if the query dictionary has an email key
+            if query.get('email') and resource == 'customers':
+                response = get_customers_by_email(query['email'][0])
                 
-        if resource == "employees":
-            if id is not None:
-                response = f"{get_single_employee(id)}"
+            if query.get('location_id') and resource == 'employees':
+                response = get_employees_by_location(query['location_id'][0])
                 
-            else:
-                response = f"{get_all_employees()}"
+            if query.get('location_id') and resource == 'animals':
+                response = get_animals_by_location(query['location_id'][0])
                 
-        if resource == "customers":
-            if id is not None:
-                response = f"{get_single_customer(id)}"
-                
-            else:
-                response = f"{get_all_customers()}"
+            if query.get('status') and resource == 'animals':
+                response = get_animals_by_status(query['status'][0])
 
-        self.wfile.write(response.encode())
+            self.wfile.write(response.encode())
         
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
@@ -154,26 +169,22 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.do_POST()
         
     def parse_url(self, path):
-        # Just like splitting a string in JavaScript. If the
-        # path is "/animals/1", the resulting list will
-        # have "" at index 0, "animals" at index 1, and "1"
-        # at index 2.
-        path_params = path.split("/")
+        """Parse the url into the resource and id"""
+        parsed_url = urlparse(path)
+        path_params = parsed_url.path.split('/')  # ['', 'animals', 1]
         resource = path_params[1]
-        id = None
 
-        # Try to get the item at index 2
+        if parsed_url.query:
+            query = parse_qs(parsed_url.query)
+            return (resource, query)
+
+        pk = None
         try:
-            # Convert the string "1" to the integer 1
-            # This is the new parseInt()
-            id = int(path_params[2])
-        except IndexError:
-            pass  # No route parameter exists: /animals
-        except ValueError:
-            pass  # Request had trailing slash: /animals/
-
-        return (resource, id)  # This is a tuple
-
+            pk = int(path_params[2])
+        except (IndexError, ValueError):
+            pass
+        return (resource, pk)
+    
     def do_DELETE(self):
         # Set a 204 response code
         self._set_headers(204)
--- views/animal_requests.py
+++ views/animal_requests.py
@@ -111,7 +111,73 @@ def get_single_animal(id):
                             data['customer_id'])
 
         return json.dumps(animal.__dict__)
-  
+    
+def get_animals_by_location(location_id):
+    with sqlite3.connect('./kennel.sqlite3') as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.breed,
+            a.status,
+            a.location_id,
+            a.customer_id
+        FROM animal a
+        WHERE a.location_id = ?
+        """, ( location_id, ))
+        
+        # Initialize an empty list to hold all animal representations
+        animals = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Animal class above.
+            animal = Animal(row['id'], row['name'], row['breed'],
+                            row['status'], row['location_id'],
+                            row['customer_id'])
+
+            animals.append(animal.__dict__)
+
+    # Use `json` package to properly serialize list as JSON
+    return json.dumps(animals)
+
+def get_animals_by_status(status):
+    with sqlite3.connect('./kennel.sqlite3') as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.breed,
+            a.status,
+            a.location_id,
+            a.customer_id
+        FROM animal a
+        WHERE a.status = ?
+        """, ( status, ))
+        animals = []
+        dataset = db_cursor.fetchall()
+        for row in dataset:
+            animal = Animal(row['id'], row['name'], row['breed'],
+                            row['status'], row['location_id'],
+                            row['customer_id'])
+
+            animals.append(animal.__dict__)
+            
+    return json.dumps(animals)
+        
 def create_animal(animal):
     # Get the id value of the last animal in the list
     max_id = ANIMALS[-1]["id"]
